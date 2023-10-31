5267513a51f8a8e5d229221164c8e19bb004db2e
Николай <nikolatrofik@gmail.com>
2023-10-31 17:48:13 +0300

    пытаюсь сделать роутинг

---
+++ quiet-overnight/app/rooms/[id].js
@@ -0,0 +1,27 @@
+import { Box, Image, Text, Link } from "@chakra-ui/react";
+
+export default function Room({ roomData }) {
+   return (
+      <Box display={'flex'} alignItems={'flex-start'} flexDirection={'column'} gap={'20px'} p={'20px'} m={'15px auto'}>
+         <Image src={roomData.imageSrc} width={500} height={500} alt={roomData.title} />
+         <Text color={'violet'}>{roomData.title}</Text>
+         <Text>{roomData.description}</Text>
+         <Link href="/">Назад</Link>
+      </Box>
+   );
+}
+
+export async function getServerSideProps({ params }) {
+   
+   const { id } = params;
+
+   const roomData = {
+      imageSrc: `/room${id}.jpg`,
+      title: 'Room Title',
+      description: 'Room description goes here.',
+   };
+
+   return {
+      props: { roomData },
+   };
+}
